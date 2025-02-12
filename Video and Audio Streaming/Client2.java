f1806a46150bbfa8efc58e2a60f007c1f19bca02
Karim <122026341+KKAARRIIMM15@users.noreply.github.com>
2025-02-12 08:34:13 +0200

    Add files via upload

---
+++ Video and Audio Streaming/Client2.java
@@ -0,0 +1,349 @@
+package com.example.jfx;
+
+
+import com.github.sarxos.webcam.Webcam;   // to access the camera
+import com.github.sarxos.webcam.WebcamResolution;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.TargetDataLine;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import java.net.ServerSocket;
+
+
+
+public class Client2 extends Application
+{
+    static Thread speakerThread = null;
+    static Thread micThread = null;
+    static Thread send_Frame_Thread = null;
+
+    static Thread recv_Frame_Thread = null;
+
+    public static void main(String[] args) {  System.out.println( System.getProperty("user.dir"));
+        launch(args);
+    }
+
+
+    private WritableImage convertToJavaFXImage(BufferedImage bufferedImage) {
+        if (bufferedImage == null)
+            return null;
+
+        int width = bufferedImage.getWidth();
+        int height = bufferedImage.getHeight();
+
+        WritableImage writableImage = new WritableImage(width, height);
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int argb = bufferedImage.getRGB(x, y);
+                pixelWriter.setArgb(x, y, argb);
+            }
+        }
+
+        return writableImage;
+    }
+
+    /*********************/
+
+    static int[][] convert_To_Pixels( BufferedImage image )
+    {
+        try{
+            // Get image dimensions
+            int width = image.getWidth();
+            int height = image.getHeight();
+
+            // Create a 2D array to store the pixel values
+            int[][] pixelArray = new int[width][height];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int pixel = image.getRGB(x, y);
+                    pixelArray[x][y] = pixel;
+                }
+            }
+            return pixelArray;
+
+        } catch ( Exception ex) {     }
+
+        return null;
+    }
+    /***************/
+
+
+    static PixelWriter Convert_To_Image( int[][] pixelArray )
+    {
+        try{
+            int width = pixelArray.length;
+            int height = pixelArray[0].length;
+
+            WritableImage writableImage = new WritableImage(width, height);
+            PixelWriter pixelWriter = writableImage.getPixelWriter();
+
+            // Set the pixel values in the BufferedImage
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int pixelValue = pixelArray[x][y];
+                    pixelWriter.setArgb(x, y, pixelValue);
+                }
+            }
+
+            return pixelWriter;
+        } catch( Exception ex ){ }
+
+        return null;
+    }
+
+    /**********************/
+
+
+    private static byte[] serialize(Serializable obj)
+    {
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
+
+            objectOutputStream.writeObject(obj);
+            objectOutputStream.flush();
+            return byteArrayOutputStream.toByteArray();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    /************************/
+
+    private static Object deserialize(byte[] serializedBytes)
+    {
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(serializedBytes);
+             ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream)) {
+
+            return objectInputStream.readObject();
+
+        } catch (Exception e) { e.printStackTrace(); }
+
+        return null;
+    }
+
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Webcam camera = Webcam.getDefault();
+        camera.setViewSize( WebcamResolution.VGA.getSize() );
+        camera.open();
+
+        ImageView imageView = new ImageView( convertToJavaFXImage( camera.getImage() ) );  imageView.setFitHeight(500);  imageView.setPreserveRatio(true);
+
+        StackPane root = new StackPane();   root.getChildren().add(imageView);
+        Scene scene = new Scene(root , 700 , 600 );  scene.setFill(Color.web("#000036"));
+
+        primaryStage.setTitle("client");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+        speakerThread = new Thread( ()->
+        {
+            SourceDataLine speaker = null;
+            try
+            {
+                AudioFormat format = new AudioFormat(48000.0f, 16, 2, true, true);
+                speaker = AudioSystem.getSourceDataLine(format);
+                speaker.open(format);
+                speaker.start();
+
+                DatagramSocket UDP_Connection = new DatagramSocket( 3333  );
+                System.out.println(" Speaker is ON ");
+                byte[] buffer = new byte[8192];
+                DatagramPacket UDP_packet = new DatagramPacket(buffer, buffer.length);
+
+                while (true)
+                {
+                    // Receive audio data from the client
+                    UDP_Connection.receive(UDP_packet);
+                    int bytesRead = UDP_packet.getLength();
+                    speaker.write(buffer, 0, bytesRead);
+
+                }
+
+            } catch ( Exception ex ){ System.out.println(" LOG:  Speaker is not recieving the correct Signal "); speaker.flush();  speaker.close(); }
+        });
+
+
+
+        micThread = new Thread( ()->
+        {
+            try
+            {
+                AudioFormat format = new AudioFormat(48000.0f, 16, 2, true, true);
+
+                // Get the microphone input
+                TargetDataLine microphone = AudioSystem.getTargetDataLine(format);
+                microphone.open(format);
+                microphone.start();
+
+                DatagramSocket UDP_Connection = new DatagramSocket(6666);
+                System.out.println(" Mic is ON ");
+
+                byte[] sig = new byte[1];
+                DatagramPacket receivePacket = new DatagramPacket( sig, sig.length);
+                UDP_Connection.receive(receivePacket);
+                System.out.println("client connected   " + receivePacket.getData()[0] );
+
+                byte[] buffer = new byte[8192];
+                while (true)
+                {
+                    // Capture audio from the microphone
+                    int bytesRead = microphone.read(buffer, 0, buffer.length);
+                    // Send the captured audio to the server
+                    DatagramPacket UDP_packet =
+                       new DatagramPacket(buffer, bytesRead, receivePacket.getAddress(), receivePacket.getPort() );
+                    UDP_Connection.send(UDP_packet);
+                }
+            } catch ( Exception ex ) { System.out.println(" LOG:  Mic is Closed ");}
+        } );
+
+
+
+        Thread UDPcameraThread = new Thread( ()->{
+            try
+            {
+                DatagramSocket UDP_Connection = new DatagramSocket(4444);
+                byte[] sig = new byte[1];
+                DatagramPacket receive_test_Packet = new DatagramPacket( sig, sig.length);
+                UDP_Connection.receive(receive_test_Packet);
+                System.out.println("client connected   " + receive_test_Packet.getData()[0] );
+
+                byte[] carry = new byte[60000];
+                DatagramPacket UDP_packet_recv_img = new DatagramPacket(carry, carry.length);
+
+            while (true)
+            {
+              try{
+                    /** send Frame  **/
+               BufferedImage image = camera.getImage();
+               ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+               ImageIO.write(image, "jpg", byteArrayOutputStream);
+               byteArrayOutputStream.flush();
+               byte[] imageBytes = byteArrayOutputStream.toByteArray();  System.out.println(imageBytes.length);
+
+         DatagramPacket UDP_packet_snd_img =
+                new DatagramPacket(imageBytes, imageBytes.length, receive_test_Packet.getAddress(), receive_test_Packet.getPort());
+                UDP_Connection.send(UDP_packet_snd_img);
+
+
+                    /** Recv Frame  **/
+                UDP_Connection.receive(UDP_packet_recv_img);
+                int bytesRead = UDP_packet_recv_img.getLength();
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                baos.write(carry, 0, bytesRead);    baos.flush();
+                byte[] actualData = baos.toByteArray();
+
+                    try{ BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(actualData));
+                        WritableImage fxImage = convertToJavaFXImage(bufferedImage);
+                        imageView.setImage(fxImage);
+                    } catch (Exception ex ){ex.printStackTrace();  System.out.println(" \n LOG: Packet Loss");}
+
+              } catch (Exception ex){ System.out.println(" \\n LOG : Poor Connection \\n \"");  }
+            }
+
+          } catch (IOException e) { System.out.println(" \n LOG :  Connection Can not be Enstablished \n ");  return; }
+        });
+
+
+        Thread UDP_recv_image_Thread = new Thread( ()->{
+            try
+            {
+                DatagramSocket UDP_Connection = new DatagramSocket(5555);
+                byte[] carry = new byte[60000];
+                DatagramPacket UDP_packet_recv_img = new DatagramPacket(carry, carry.length);
+
+                while (true)
+                {
+                    try{
+                        /** Recv Frame  **/
+                        UDP_Connection.receive(UDP_packet_recv_img);
+                        int bytesRead = UDP_packet_recv_img.getLength();
+
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        baos.write(carry, 0, bytesRead);    baos.flush();
+                        byte[] actualData = baos.toByteArray();
+
+                        try{ BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(actualData));
+                            WritableImage fxImage = convertToJavaFXImage(bufferedImage);
+                            imageView.setImage(fxImage);
+                        } catch (Exception ex ){ex.printStackTrace();  System.out.println(" \n LOG: Packet Loss");}
+
+                    } catch (Exception ex){ System.out.println(" \\n LOG : Poor Connection \\n \"");  }
+                }
+
+            } catch (IOException e) { System.out.println(" \n LOG :  Connection Can not be Enstablished \n ");  return; }
+        });
+
+
+        Thread UDP_send_image_Thread = new Thread( ()->{
+            try
+            {
+                DatagramSocket UDP_Connection = new DatagramSocket(4444);
+                byte[] sig = new byte[1];
+                DatagramPacket receive_test_Packet = new DatagramPacket( sig, sig.length);
+                UDP_Connection.receive(receive_test_Packet);
+                System.out.println("client connected   " + receive_test_Packet.getData()[0] );
+
+                while (true)
+                {
+                    try{
+                        /** send Frame  **/
+                        BufferedImage image = camera.getImage();
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                        ImageIO.write(image, "jpg", byteArrayOutputStream);
+                        byteArrayOutputStream.flush();
+                        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+                        DatagramPacket UDP_packet_snd_img =
+                                new DatagramPacket(imageBytes, imageBytes.length, receive_test_Packet.getAddress(), receive_test_Packet.getPort());
+                        UDP_Connection.send(UDP_packet_snd_img);
+
+                    } catch (Exception ex){ System.out.println(" \\n LOG : Poor Connection \\n \"");  }
+                }
+
+            } catch (IOException e) { System.out.println(" \n LOG :  Connection Can not be Enstablished \n ");  return; }
+        });
+
+
+UDP_recv_image_Thread.start();
+UDP_send_image_Thread.start();
+speakerThread.start();
+micThread.start();
+    }
+
+}
